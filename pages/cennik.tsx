79e2d304a4b6953804f21f0c3c5f14048d91ba94
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2022-01-25 17:33:27 +0100

    update pricelist v2

--- pages/cennik.tsx
+++ pages/cennik.tsx
@@ -107,7 +107,7 @@ function Cennik() {
           <tbody>
             <tr>
               <td>BRWI - OMBRE BROWS</td>
-              <td>800 zł</td>
+              <td>900 zł</td>
             </tr>
           </tbody>
         </StyledTable>
