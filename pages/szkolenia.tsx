855336c4ad8f327a05179738b361f6c232241a06
Pawel Nackiewicz <it.pnackiewicz@gmail.com>
2022-03-26 09:33:31 +0100

    change szkolenia redirect page

--- pages/szkolenia.tsx
+++ pages/szkolenia.tsx
@@ -89,7 +89,7 @@ function Szkolenia() {
           </StyledSectionCenter>
         </StyledArticle>
         <StyledLinkParagraph
-          href="https://rarityacademy.pl/natalia-golomb"
+          href="https://rarityacademy.pl/instruktorka/natalia-golomb"
           target="_blank"
           rel="noreferrer"
         >
